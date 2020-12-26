dd14eb7df1a9a7b3dfb9fb9c57d14f641fbd1a7c
Alice Genkin <agenkin1@gmail.com>
2020-12-26 14:52:41 -0500

    add loggd_in and current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,20 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  helpers do 
+
+    def logged_in?
+      !!current_user
+      #takes a value and returns boolean reflection of its truthiness
+    end
+
+    def current_user
+      @current_user ||= User.find_by[id: session[:user_id]]
+      #if find doesn't return what it's looking for it returns error which is why we use find_by
+     #find_by is an activerecord method that is going to query the database
+     #using @ reduces the number of database calls
+     #or equals || will create and assign if the current user is found
+    end
+    #helpers are available through views and controllers
+  end 
 end
