2bffc6437ca92a9147f7e2b82f935eda51de05fd
Alice Genkin <agenkin1@gmail.com>
2021-01-13 20:09:18 -0500

    login error handling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,19 @@
 class UsersController < ApplicationController
-  get '/login' do
+  
+get '/login' do
   redirect_if_logged_in
   erb :login
 end
 
-get '/users' do
-  erb :signup
-end
+# get '/users' do
+#   erb :signup
+# end
 
 #the purpose of this route is to receive the login form, find hte user, and log the user in (create a session)
 post '/login' do
  #find the user 
-   @user = User.find_by(email: params[:email]) 
-    if @user && @user.authenticate(params[:password])
+  @user = User.find_by(email: params[:email]) 
+  if @user && @user.authenticate(params[:password])
     # SET sessions[:user_id] value here
     # session[:user_id] = 1
     # x = 1
@@ -27,38 +28,36 @@ post '/login' do
   end
 end
 
-  get '/signup' do
-    erb :signup
-  end
+get '/signup' do
+  erb :signup
+end
 
-  post '/users' do
-    #create the user and persist the user to the database
-  if params[:name] != "" && params[:email] != "" && params[:password] != "" 
+post '/users' do
+  #create the user and persist the user to the database
   @user=User.create(params) 
   #creates user and persists to the database
   if @user.save
-  session[:user_id] = @user.id #logs the user in 
-  # go to the user show page
-  redirect "/users/#{@user.id}"
-else 
+    session[:user_id] = @user.id #logs the user in 
+    # go to the user show page
+    redirect "/users/#{@user.id}"
+  else  
     #if not valid input, take them back to signup 
     flash[:errors] = "Your credentials were invalid.  Please sign up or try again."
-    erb :'/signup'
-end
-end
+    redirect '/signup'
+  end
 end 
   
-  #user SHOW route
-  get '/users/:id' do
-    redirect_if_not_logged_in
-    @user=User.find_by(id: params[:id])
-    #can either log them in or send them back to login page
-    erb :'/users/show'
-  end
+#user SHOW route
+get '/users/:id' do
+  redirect_if_not_logged_in
+  @user=User.find_by(id: params[:id])
+  #can either log them in or send them back to login page
+  erb :'/users/show'
+end
 
-  get '/logout' do
-    session.clear
-    redirect '/'
-  end
+get '/logout' do
+  session.clear
+  redirect '/'
+end
   
 end
