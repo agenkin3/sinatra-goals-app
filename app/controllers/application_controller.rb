d66ee404805a1319d1210d3fa3e44d93d06e3e8a
Alice Genkin <agenkin1@gmail.com>
2021-01-14 14:07:02 -0500

    combine helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -49,9 +49,12 @@ class ApplicationController < Sinatra::Base
       if logged_in?
         redirect "/users/#{current_user.id}"
       end
-
       #def redirect_if_logged_in_as_someone_else
       #end
+
+      def set_goal_entry
+        @goal_entry = GoalEntry.find(params[:id])
+      end
     end
   end
 end
